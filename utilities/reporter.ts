070c2c6abf0fa9fdf19d50a6bfea388d28c52c09
Yurii Veremienko <i0638143049@gmail.com>
2018-07-01 18:05:44 +0300

    fix due to api change

--- utilities/reporter.ts
+++ utilities/reporter.ts
@@ -1,10 +1,10 @@
-import { CucumberJSAllureFormatter, AllureRuntime } from 'cucumberjs-allure2-reporter';
+import { CucumberJSAllureFormatter, AllureRuntime, AllureConfig } from 'cucumberjs-allure2-reporter';
 
 export default class Reporter extends CucumberJSAllureFormatter {
 	constructor(options: any) {
 		super(
 			options,
-			new AllureRuntime({ resultsDir: './reports/allure-results' }),
+			new AllureRuntime(new AllureConfig( './reports/allure-results')),
 			{
 				labels: {
 					issue: [/@bug_(.*)/],
